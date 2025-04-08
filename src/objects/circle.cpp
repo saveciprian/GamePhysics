1422a3ef3af0fb531f69b8c4f8e4fe2cd82fa572
Tudor Savescu <60888561+saveciprian@users.noreply.github.com>
2025-04-08 11:48:52 +0200

    added in force field & empty objects

--- src/objects/circle.cpp
+++ src/objects/circle.cpp
@@ -1,12 +1,13 @@
 #include "circle.h"
 #include "core/Draw.h"
 #include <iostream>
+#include <math.h>
 
 Circle::Circle(glm::vec2 pos)
     : position(0, 4),
       circleRadius(1),
       velocity(0, 0),
-      acceleration(0, -9.81),
+      acceleration(0, 0),
       deltaTime(0) {
 
     position = pos;
@@ -15,8 +16,19 @@ Circle::Circle(glm::vec2 pos)
 Circle::~Circle() {}
 
 void Circle::CalculateAcceleration() {
-    //should add forces here I think? If there are no forces acting on the object it should only respond to gravity; maybe also drag?
+    acceleration = glm::vec2(0, 0);
 
+
+    //should add forces here I think? If there are no forces acting on the object it should only respond to gravity; maybe also drag?
+    glm::vec2 gravity = glm::vec2(0, -9.81);
+    acceleration += gravity;
+
+    for (int i = 0; i < forceFields.size(); i++) {
+        acceleration += forceFields[i];
+        
+    }
+
+    forceFields.clear();
 
 }
     
@@ -24,21 +36,41 @@ void Circle::CalculateVelocity() {
     velocity = velocity + acceleration * deltaTime;
 }
 
+int Circle::getMangitude(glm::vec2 vec)
+{
+    return sqrt(pow(vec[0], 2) + pow(vec[1], 2));
+}
+
 void Circle::PhysicsStep(float dt) {
     deltaTime = dt; //set object's value for delta time
     CalculateAcceleration();
     CalculateVelocity();
+    position = position + velocity * deltaTime;
 
-    if (position[1] < 0 + circleRadius) {
-        position[1] = 0 + circleRadius;
-        velocity = velocity * glm::vec2(-1, -1);
+    glm::vec2 forceField(-1, 0);
+    if (getMangitude(forceField - position) < 1 + circleRadius)
+    {
+        glm::vec2 force = position - forceField;
+        int scalar = 10;
+        forceFields.push_back(glm::vec2(force[0] * scalar, force[1] * scalar));
+        std::cout << "Force: " << (position - forceField)[0] << ", "
+                  << (position - forceField)[1] << std::endl;
+        
     }
 
-    position = position + velocity * deltaTime;
+    if (position[1] < 0 + circleRadius) {
+        /*int i = abs(position[i]);*/
+
+
+        position[1] = 0 + circleRadius;
+        velocity = velocity * glm::vec2(1, -1);
+    }
+
 }
 
 void Circle::Draw()
 {
+    Draw::Circle(glm::vec2(-1, 0), 1);
     Draw::Circle(position, circleRadius);
 }
 
--- src/objects/circle.h
+++ src/objects/circle.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <glm/glm.hpp>
+#include <vector>
 
 class Circle {
 public:
@@ -12,11 +13,13 @@ public:
     virtual void Collide();
     virtual void Draw();
     virtual const char* GetName() { return "Circle Object"; };
+    int Circle::getMangitude(glm::vec2 vec);
     glm::vec2 position;
     float circleRadius;
     glm::vec2 velocity;
     glm::vec2 acceleration;
     float deltaTime;
+    std::vector<glm::vec2> forceFields;
 
 private:
 
